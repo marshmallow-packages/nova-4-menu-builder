ef5f0e806863741ab3b656e7d6ea531618799576
LTKort <lars.kort94@gmail.com>
2025-08-18 11:39:29 +0200

    builder

--- resources/js/components/MenuBuilder.vue
+++ resources/js/components/MenuBuilder.vue
@@ -8,7 +8,7 @@
         <div class="item-data o1-items-center o1-flex o1-shrink o1-min-w-0" :class="{ 'o1-px-3': !hasChildren(item) }">
           <button v-if="hasChildren(item)" @click.prevent="toggleMenuChildrenCascade(item)"
             class="o1-appearance-none o1-cursor-pointer o1-fill-current hover:o1-text-primary o1-flex o1-px-3 o1-items-center focus:o1-outline-none">
-            <Icon :type="isCascadeOpen(item) ? 'chevron-down' : 'chevron-up'" />
+            <Icon :name="getIconName(item)" type="outline" />
           </button>
 
           <div class="o1-text-90 o1-font-bold o1-whitespace-nowrap o1-overflow-hidden o1-text-ellipsis"
@@ -24,20 +24,20 @@
         </div>
 
         <div class="buttons o1-gap-x-2 md:o1-w-1/3 o1-flex o1-justify-end o1-content-center">
-          <button :title="__('novaMenuBuilder.edit')" @click.prevent="$emit('editMenu', item)"
-            class="text-gray-500 dark:text-gray-400 hover:[&:not(:disabled)]:text-primary-500 dark:hover:[&:not(:disabled)]:text-primary-500">
-            <Icon type="pencil-alt" />
-          </button>
+          <span :title="__('novaMenuBuilder.edit')" @click.prevent="$emit('editMenu', item)"
+            class="mt-2 cursor-pointer text-gray-500 dark:text-gray-400 hover:[&:not(:disabled)]:text-primary-500 dark:hover:[&:not(:disabled)]:text-primary-500">
+            <Icon name="PencilAlt" type="outline" />
+          </span>
 
-          <button :title="__('novaMenuBuilder.duplicate')" @click.prevent="$emit('duplicateMenuItem', item)"
-            class="text-gray-500 dark:text-gray-400 hover:[&:not(:disabled)]:text-primary-500 dark:hover:[&:not(:disabled)]:text-primary-500">
-            <Icon type="duplicate" />
-          </button>
+          <span :title="__('novaMenuBuilder.duplicate')" @click.prevent="$emit('duplicateMenuItem', item)"
+            class="mt-2 cursor-pointer text-gray-500 dark:text-gray-400 hover:[&:not(:disabled)]:text-primary-500 dark:hover:[&:not(:disabled)]:text-primary-500">
+            <Icon name="Duplicate" type="outline" />
+          </span>
 
-          <button :title="__('novaMenuBuilder.delete')" @click.prevent="$emit('removeMenu', item)"
-            class="text-gray-500 dark:text-gray-400 hover:[&:not(:disabled)]:text-primary-500 dark:hover:[&:not(:disabled)]:text-primary-500">
-            <Icon type="trash" />
-          </button>
+          <span :title="__('novaMenuBuilder.delete')" @click.prevent="$emit('removeMenu', item)"
+            class="mt-2 cursor-pointer text-gray-500 dark:text-gray-400 hover:[&:not(:disabled)]:text-primary-500 dark:hover:[&:not(:disabled)]:text-primary-500">
+            <Icon name="trash" type="outline" />
+          </span>
         </div>
       </vue-nestable-handle>
     </template>
@@ -46,6 +46,7 @@
 
 <script>
 import { VueNestable, VueNestableHandle } from 'vue3-nestable';
+import { Icon } from 'laravel-nova-ui';
 
 export default {
   props: {
@@ -59,10 +60,15 @@ export default {
       default: 10,
     },
   },
-
+  computed: {
+    getIconName() {
+      return item => (this.isCascadeOpen(item) ? 'chevron-down' : 'chevron-up');
+    },
+  },
   components: {
     VueNestable,
     VueNestableHandle,
+    Icon,
   },
 
   data: () => ({
